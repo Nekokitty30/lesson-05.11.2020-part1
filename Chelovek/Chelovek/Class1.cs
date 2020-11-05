003fc768ac248bc73ea4c38c094e2ef586ce680f
Nekokitty30 <72338825+Nekokitty30@users.noreply.github.com>
2020-11-05 23:50:33 +0300

    Add files via upload

---
+++ Chelovek/Chelovek/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Chelovek
+{
+    public class Chelovek1
+    {
+        private string _mood = "norm";
+        /*private string _name;*/
+       /* private int _age;*/
+        private int _pulse=60;
+
+
+        public string Mood { get; set; }
+        public string Name { get; private set; }
+
+        private int _age;
+        public int Age {
+            get
+            {
+                return _age + 10;
+            }
+                 set
+            { if (value <= 114)
+                { _age = value; 
+                } else
+                { throw new Exception(); 
+                }
+            }
+        }
+
+        public Chelovek1(string name,int age)
+        {
+            Name = name;
+            _age = age;
+        }
+        public void TellName()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public void Run(int km)
+        {
+            Console.WriteLine("Я бегу");
+            _pulse += km;
+            Mood = "устал";
+
+        }
+
+
+        public void Relax(int time)
+        {
+            Console.WriteLine("Я отдыхаю");
+            _pulse -= time;
+            Mood = "xorosho";
+        }
+        public void GetPulse()
+        {
+
+            Console.WriteLine(_pulse);
+        }
+       /* public void AskName(Chelovek1 ch)
+        {  Console.WriteLine($"Я-{Name}.Спросил как зовут{ch.name}");
+
+        }*/
+    }
+}
---
+++ Chelovek/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Chelovek;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Chelovek1 gosha = new Chelovek1();
+            /*gosha.TellName();
+            Console.WriteLine(gosha.age);*/
+
+            /*gosha.name = "Gosha";
+            gosha.age = 7;
+            gosha.TellName();
+
+            Chelovek1 petia = new Chelovek1();
+            petia.name = "Petia";
+            petia.age = 7;
+            petia.TellName();
+            petia.AskName(gosha);*/
+            /*  Chelovek1 gosha = new Chelovek1();
+              gosha.GetPulse();
+              gosha.Run(15);
+              gosha.pulse = 0;
+              gosha.GetPulse();*/
+            Chelovek1 gosha = new Chelovek1("gosha", 10);
+            gosha.TellName();
+            Console.WriteLine(gosha.Name);
+            gosha.Age = 144;
+            Console.WriteLine(gosha.Age);
+
+
+        }
+    }
+}
